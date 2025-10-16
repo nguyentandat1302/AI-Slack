b7594ecc8ace940b2a4a6c713b1ec922010ce37c
RuanChenDa <21280103e0017@student.tdmu.edu.vn>
2025-10-16 15:39:09 +0700

    wtf

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import SlackChat from './components/SlackChat.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    @/assets/logo.svg
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <SlackChat />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
